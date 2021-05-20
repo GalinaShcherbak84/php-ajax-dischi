24c12cc34a1dd20e8a686121dc2bb024806dd06a
GalinaSherbak84 <galinasherbak@gmail.com>
2021-05-20 17:05:24 +0200

    cards

--- php/index.php
+++ php/index.php
@@ -7,6 +7,9 @@
     <title>Document</title>
     <!--  style -->
     <link rel="stylesheet" href="./style.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;1,300&display=swap" rel="stylesheet">
 </head>
 <body>
    <?php
@@ -14,11 +17,24 @@
         //var_dump($database);
     ?>
     <div class="app">
+        <!-- header -->
         <header>
             <img src="./php-dischi/logo.png" alt="">
         </header>
+        <!-- main -->
         <div class="container">
+            <!-- disco -->
             
+                <?php foreach($database as $card){ ?>
+                    <div class="card">
+                    <img src="<?php echo $card['poster']; ?>" alt="">
+                    <h3> <?php echo $card['title']; ?> </h3>
+                    <p> <?php echo $card['author']; ?> </p>
+                    <h3> <?php echo $card['year']; ?> </h3>
+                    <p> <?php echo $card['genre']; ?> </p>
+                    </div>
+                <?php } ?>
+                
         </div>
     </div>
 </body>
